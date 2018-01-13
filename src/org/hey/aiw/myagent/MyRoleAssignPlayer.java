44fc67282b0aa6ca6460454b62af370a01ac675c
Hey8 <ooshio@gmail.com>
2018-01-13 17:36:02 +0900

    設定ファイル

--- src/org/hey/aiw/myagent/MyRoleAssignPlayer.java
+++ src/org/hey/aiw/myagent/MyRoleAssignPlayer.java
@@ -4,6 +4,15 @@ import org.aiwolf.sample.lib.AbstractRoleAssignPlayer;
 
 public class MyRoleAssignPlayer extends AbstractRoleAssignPlayer {
 
+	public MyRoleAssignPlayer(){
+		setVillagerPlayer(new MyVillager());
+		setBodyguardPlayer(new MyBodyguard());
+		setMediumPlayer(new MyMedium());
+		setSeerPlayer(new MySeer());
+		setPossessedPlayer(new MyPossessed());
+		setWerewolfPlayer(new MyWerewolf());
+	}
+
 	@Override
 	public String getName() {
 		return "MyRoleAssignPlayer";
